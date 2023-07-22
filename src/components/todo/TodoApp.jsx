b76314c1bb93ae854b5968f496fa6e39fbd862c5
kaifmohd75 <kaifmohd75@gmail.com>
2023-07-23 00:26:19 +0530

    Started with todo app created and hardcoded the login authentication and navigating to Welcome page using useNavigate hook  and also using Routes from React-route-dom

--- src/components/todo/TodoApp.jsx
+++ src/components/todo/TodoApp.jsx
@@ -1,37 +1,115 @@
+import { useState } from 'react'
+import { BrowserRouter,Routes,Route, useNavigate,useParams } from 'react-router-dom'
+import './TodoApp.css'
+
+
 export default function TodoApp(){
     return (
         <div className="TodoApp">
-            Todo Management Application
-            <LoginComponent />
-            {/* <WelcomeComponent /> */}
+            <BrowserRouter>
+                <Routes>
+                    <Route path='/' element={<LoginComponent />}></Route>
+                    <Route path='/login' element={<LoginComponent />}></Route>
+                    <Route path='/welcome/:username' element={<WelcomeComponent />}></Route>
+                    <Route path='*' element={<ErrorComponent />}></Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     )
 }
 
 function LoginComponent(){
+
+    const [username,setUsername] = useState('kaif')
+    const [password,setPassword] = useState('')
+    const [showSuccessMessage,setShowSuccessMessage] = useState(false)
+    const [showErrorMessage,setShowErrorMessage] = useState(false)
+    
+    const navigate = useNavigate()
+    
+    function handleUsernameChange(event){
+        setUsername(event.target.value);
+    }
+
+    function handlePasswordChange(event){
+        setPassword(event.target.value);
+    }
+
+    function handleSubmit(){
+        if(username==='kaif' && password==='dummy'){
+            setShowSuccessMessage(true);
+            setShowErrorMessage(false);
+            navigate(`/welcome/${username}`)
+        }else{
+            setShowSuccessMessage(false)
+            setShowErrorMessage(true)
+        }
+        
+    }
+    // function SuccessMessageComponent(){
+    //     if(showSuccessMessage){
+    //         return <div className='successMessage'>Authenticated Successfully</div>
+    //     }
+    //     return null;
+    // }
+    // function ErrorMessageComponent(){
+    //     if(showErrorMessage){
+    //         return <div className='errorMessage'>Authenticated Failed. Please Check your Credentials</div>
+    //     }
+    //     return null;
+    // }
+
     return (
         <div className="Login">
+             <h1>Time to Login!</h1>
+
+            {showSuccessMessage && <div className='successMessage'>Authenticated Successfully</div>}
+            {showErrorMessage && <div className='errorMessage'>Authenticated Failed. Please Check your Credentials</div>}
+            {/* <SuccessMessageComponent />
+            <ErrorMessageComponent /> */}
+            
             <div className="LoginForm">
                 <div>
                     <label>User Name</label>
-                    <input type="text" name="username" />
+                    <input type="text" name="username" value={username} onChange={handleUsernameChange}/>
                 </div>
                 <div>
                     <label>Password</label>
-                    <input type="password" name="password" />
+                    <input type="password" name="password" value={password} onChange={handlePasswordChange} />
                 </div>
                 <div>
-                    <button type="button" name="login">Login</button>
+                    <button type="button" name="login" onClick={handleSubmit}>Login</button>
                 </div>
             </div>
         </div>
     )
 }
 
+
+
 function WelcomeComponent(){
+
+    const {username} = useParams()
+
+
     return (
-        <div className="Welcome">
+        <div className="WelcomeComponent">
+        <h1>Welcome {username}</h1>
+        <div >
             Welcome Component
+        </div>
+        </div>
+    )
+}
+
+
+function ErrorComponent(){
+    return (
+        <div className="ErrorComponent">
+            <h1>We are working really hard!</h1>
+            <div>
+                Apologies for the 404. Reach out to our team at ABC-DEF-GHI
+            </div>
         </div>
     )
 }
